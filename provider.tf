2bfa04b59faa66ff337a7710bb86a8be4221759f
Kim Yongbeom <ghzx55@naver.com>
2025-05-25 18:03:53 +0900

    k8s, helm providers 분리 (#45)

--- provider.tf
+++ provider.tf
@@ -19,21 +19,21 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 # provider "kubernetes" {
-#   config_path = pathexpand("~/.kube/config")
+#   host                   = aws_eks_cluster.this.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
 # }
 
-# Helm Chart Repository 추가
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+# # provider "kubernetes" {
+# #   config_path = pathexpand("~/.kube/config")
+# # }
+
+# # Helm Chart Repository 추가
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.this.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
